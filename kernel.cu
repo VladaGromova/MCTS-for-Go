ce65022157534403779d0a309bdc13f5778d8dab
VladaGromova <vladagromova1106@gmail.com>
2024-01-17 22:36:56 +0100

    cuda working

--- kernel.cu
+++ kernel.cu
@@ -15,8 +15,8 @@
 #define SIZE 9
 #define NUM_OF_MOVEMENTS_IN_SIMULATION 10
 #define MAX_DEPTH 5 // tyle razy wykonamy te 3 etapy
-#define MOVEMENTS 6
-#define MAX_NUMBER_OF_THREADS 256
+#define MOVEMENTS 40
+#define MAX_NUMBER_OF_THREADS 512
 
 enum State { EMPTY, BLACK, WHITE };
 
